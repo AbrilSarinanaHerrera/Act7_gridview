593ba2c5e729891f4a2173db314271c0c0a05167
User_T7 <a.20308051280553@cbtis128.edu.mx>
2023-04-20 22:18:20 +0000

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,31 +12,133 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Hello World',
       // Application theme data, you can set the colors for the application as
       // you want
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Floreria Sariñana'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        appBar: AppBar(
+          // The title text which will be shown on the action bar
+          title: Text(title),
         ),
-      ),
-    );
+        body: GridView.extent(
+          maxCrossAxisExtent: 200,
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
+          children: [
+            Container(
+                color: const Color(0xff580f3a),
+                child: Column(
+                  children: [
+                    Image.network(
+                      'https://cdn-icons-png.flaticon.com/128/2608/2608993.png',
+                    ),
+                    const Text(
+                      "Arreglo 1",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                )),
+            Container(
+                color: const Color(0xff580f3a),
+                child: Column(
+                  children: [
+                    Image.network(
+                      'https://cdn-icons-png.flaticon.com/128/3095/3095126.png',
+                    ),
+                    const Text(
+                      "Arreglo 2",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                )),
+            Container(
+                color: const Color(0xff580f3a),
+                child: Column(
+                  children: [
+                    Image.network(
+                        'https://cdn-icons-png.flaticon.com/128/3095/3095126.png'),
+                    const Text(
+                      "Arreglo 3",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                )),
+            Container(
+                color: const Color(0xff580f3a),
+                child: Column(
+                  children: [
+                    Image.network(
+                        'https://cdn-icons-png.flaticon.com/128/2608/2608993.png'),
+                    const Text(
+                      "Arreglo 4",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                )),
+            Container(
+                color: const Color(0xff580f3a),
+                child: Column(
+                  children: [
+                    Image.network(
+                        'https://cdn-icons-png.flaticon.com/128/3095/3095126.png'),
+                    const Text(
+                      "Arreglo 5",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                )),
+            Container(
+                color: const Color(0xff580f3a),
+                child: Column(
+                  children: [
+                    Image.network(
+                        'https://cdn-icons-png.flaticon.com/128/3095/3095126.png'),
+                    const Text(
+                      "Arreglo 6",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                )),
+            Container(
+                color: const Color(0xff580f3a),
+                child: Column(
+                  children: [
+                    Image.network(
+                        'https://cdn-icons-png.flaticon.com/128/3095/3095126.png'),
+                    const Text(
+                      "Arreglo 7",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                )),
+            Container(
+                color: const Color(0xff580f3a),
+                child: Column(
+                  children: [
+                    Image.network(
+                        'https://cdn-icons-png.flaticon.com/128/2608/2608993.png'),
+                    const Text(
+                      "Arreglo 8",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                )),
+          ],
+          padding: const EdgeInsets.all(15),
+          shrinkWrap: true,
+        ));
   }
 }
